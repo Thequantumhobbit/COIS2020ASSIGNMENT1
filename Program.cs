8a7b9c92737b41d82d547a5db26183ad1fb53c77
LadyRootBeer <59972345+LadyRootBeer@users.noreply.github.com>
2020-10-17 12:58:20 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-﻿
 using System;
 using System.Collections.Generic;
 //using System.Data;
@@ -16,44 +15,62 @@ namespace COIS2020Assignment1
 
 
 
-            Console.WriteLine("Please enter the coefficent for the first term in the polynomial"); // user input for the coefficient of a new term 
-            double coeff = Convert.ToDouble(Console.ReadLine()); 
+            Console.WriteLine("Please enter the coefficent for the first term in the polynomial");
+            double coeff = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Please enter the exponent for the first term in the polynmial"); // user input for the exponent 
+            Console.WriteLine("\n");
+            Console.WriteLine("Please enter the exponent for the first term in the polynmial");
             int exp = Convert.ToInt32(Console.ReadLine());
 
-            Term Term1 = new Term(coeff, exp); // create a term with the user inputted values 
+            Term Term1 = new Term(coeff, exp);
 
-            Console.WriteLine("Please enter the value at which you would like your polynomail evaluated at"); // user input for the value that the term will be evaluated at
+            Console.WriteLine("\n");
+            Console.WriteLine("Please enter the value at which you would like your polynomail evaluated at");
             double y = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("The current term in the polynomial is: " + Term1.ToString()); //  print out term1, the user inputted value 
 
-            Console.WriteLine("This term evaluates to: " + Term1.Evaluate(y)); // Evalute term1 at the user inputted value 
+            
 
+            Console.WriteLine("The current term in the polynomial is: " + Term1.ToString());
 
-            Term test2 = new Term(-2.2, 2); // create three new terms to run tests with 
+            
+            Console.WriteLine("This term evaluates to: "+Term1.Evaluate(y));
+
+            Term test2 = new Term(-2.2, 2);
             Term test1 = new Term(1.1, 1);
             Term test3 = new Term(3.3, 3);
 
-            Polynomial Poly1 = new Polynomial(); // Initalize the polynomial poly1
+            
 
-            Poly1.AddTerm(test2); // Add the three terms to the polynomial poly1
-            Poly1.AddTerm(test1);
-            Poly1.AddTerm(test3);
+            Polynomial Poly1 = new Polynomial();
 
-            Term test4 = new Term(4.4, 4); // create an additional term to run tests with 
+            
 
-            Poly1.AddTerm(test4); // add the 4th term to the polynomial 
+           Poly1.AddTerm(test2);
+           Poly1.AddTerm(test1);
+           Poly1.AddTerm(test3);
 
-            Poly1.Print(); //  print out poly1 
+           
 
-          
 
-            Console.WriteLine("Please enter the value at which you would like to be evaluate with"); // enter the value that you would like to evaluate poly1 at
+           
+
+            Term test4 = new Term(4.4, 4);
+
+            Poly1.AddTerm(test4);
+
+            
+
+            Poly1.Print();
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Please enter the value at which you would like to be evaluate with");
             double z = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("This term evaluates to: " + Poly1.Evaluate(z)); // print out poly1 evaluated at z
+
+
+            Console.WriteLine("This term evaluates to: " + Poly1.Evaluate(z));
 
 
 
@@ -62,41 +79,58 @@ namespace COIS2020Assignment1
 
 
 
-            Term addtest1 = new Term(5.5, 5); // create an additional 3 test terms 
-            Term addtest2 = new Term(6.6, 6);
+            Term addtest1 = new Term(5.5, 5);
+            Term addtest2= new Term(6.6, 6);
             Term addtest3 = new Term(1.5, 2);
 
-            Polynomial Poly2 = new Polynomial(); // create an additional polynomial poly2
 
-            Poly2.AddTerm(addtest2); // add test terms to poly2 
+
+            Polynomial Poly2 = new Polynomial();
+
+
+
+            Poly2.AddTerm(addtest2);
             Poly2.AddTerm(addtest1);
             Poly2.AddTerm(addtest3);
 
-            Poly2.Print(); // print out poly 2
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
 
-            Polynomial Poly3 = new Polynomial(); // create poly3 to hold the new polynomial created by the addition of poly 1 and 2
-            Poly3 = Poly1 + Poly2; // add poly 1 and 2 and store in poly 3
+           
+            Poly2.Print();
+            Console.WriteLine("\n");
 
-            Poly3.Print(); //  print out poly 3
+            Polynomial Poly3 = new Polynomial();
+            Poly3 = Poly1 + Poly2;
 
-            Console.WriteLine("/n");
+            Poly3.Print();
+
+
+
+            Console.WriteLine("\n");
 
             Polynomial Poly4 = new Polynomial();
             Polynomial Poly5 = new Polynomial();
             Polynomial Poly6 = new Polynomial();
 
-            Term multiplytest1 = new Term(5, 2);
-            Term multiplytest2 = new Term(1, 1);
-            Term multiplytest3 = new Term(3, 3);
-            Term multiplytest4 = new Term(3, 2);
+            Term multiplytest1 = new Term(5.0, 2);
+            Term multiplytest2 = new Term(1.0, 4);
+            Term multiplytest3 = new Term(3.0, 3);
+            Term multiplytest4 = new Term(3.0, 2);
 
             Poly5.AddTerm(multiplytest1);
             Poly5.AddTerm(multiplytest2);
 
+            Poly5.Print();
+            Console.WriteLine("\n");
+
             Poly6.AddTerm(multiplytest3);
             Poly6.AddTerm(multiplytest4);
 
-            Poly4 = Poly5 * Poly6; 
+            Poly6.Print();
+            Console.WriteLine("\n");
+
+            Poly4 = Poly5 * Poly6;
             Poly4.Print();
 
         }
@@ -157,9 +191,9 @@ namespace TermCreation
 
             }
 
-
-           
-
+            
+            Console.WriteLine("This is the new term you created: "+ Coefficient + "x" + "^" + Exponent);
+            
 
 
 
@@ -203,7 +237,7 @@ namespace TermCreation
         public override string ToString()
         {
 
-            // double y = this.Atx;
+           // double y = this.Atx;
             return Coefficient + "x" + "^" + Exponent;
         }
     }
@@ -231,25 +265,25 @@ namespace Task3
 
     public class Polynomial
     {
-        // private int capacity;          // Maximum number of items in a list
+       // private int capacity;          // Maximum number of items in a list
         private int count; // Number of items in the list
         private int next = 0;
         private Term[] P;            // Array of items 
-                                     // Items are stored beginning at position 0
+                    // Items are stored beginning at position 0
 
         public Polynomial() // Creates the polynomial array 
         {
-            P = new Term[50];
-
+           P = new Term[50];
+            
             count = 0;
 
 
             P[0] = new Term(0.0, 0);
             next++;
-
+            
         }
 
-
+       
 
         public int GetCount()
         {
@@ -277,29 +311,29 @@ namespace Task3
                 Grow(g);
             }
             */
-
+            
 
             P[next] = term;
             next++;
             count++;
 
-
+            
             int i = count;
-            int j = i - 1;
+            int j = i-1;
             Term current = P[i];
             Term after = P[j];
 
+            
 
-
-            while (j != -1)
+            while (j!=-1)
             {
-
+                
                 current = P[i];
                 after = P[j];
 
                 if (current.GetExpo() > after.GetExpo())
                 {
-
+                    
                     int temp = current.GetExpo();
                     current.SetExpo(after.GetExpo());
                     after.SetExpo(temp);
@@ -308,11 +342,14 @@ namespace Task3
                     current.SetCoeff(after.GetCoeff());
                     after.SetCoeff(temp2);
 
+                    
+                   
+
                 }
 
                 ////////////////////
-
-                if (current.GetExpo() == after.GetExpo())
+                
+                 if (current.GetExpo() == after.GetExpo())
                 {
 
                     double temp1 = after.GetCoeff();
@@ -320,36 +357,39 @@ namespace Task3
                     double temp3 = temp1 + temp2;
                     after.SetCoeff(temp3);
 
-                    for (int k = i; k < count - 1; k++)
+                   for (int k = i; k<count-1;k++)
                     {
                         P[k] = P[k + 1];
-
+                       
                     }
+
+                       
+
 
                 }
 
                 else
-
-                    //////////////////
+                
+                //////////////////
 
                     i--;
-                j--;
+                    j--;
 
+                   
+                }
 
-            }
+                
 
-
-
-
+            
 
         }
 
 
         public static Polynomial operator +(Polynomial p, Polynomial q)
         {
-            Polynomial hold = new Polynomial();
+             Polynomial hold=new Polynomial();
 
-            for (int i = 0; i <= p.GetCount(); i++)
+            for (int i=0; i<= p.GetCount();i++)
             {
                 Term temp = new Term(p.P[i].GetCoeff(), p.P[i].GetExpo());
 
@@ -366,20 +406,42 @@ namespace Task3
             return hold;
         }
 
+
+
+        public double Evaluate(double x)
+        {
+            double answer=0;
+
+
+            for (int i = 0; i <= count; i++)
+            {
+
+               
+                Double termans = Math.Pow(x, P[i].GetExpo()) * P[i].GetCoeff();
+                answer += termans;
+            }
+            
+
+
+
+            return answer;
+        }
+
+
         public static Polynomial operator *(Polynomial p, Polynomial q)
         {
             Polynomial hold2 = new Polynomial();
-            double tempC=0, coeffp =0, coeffq=0;
-            int tempE = 0, expop = 0, expoq =0 ;
-            
-            for( int i  = 0; i <=p.GetCount(); i++)
+            double tempC = 0, coeffp = 0, coeffq = 0;
+            int tempE = 0, expop = 0, expoq = 0;
+
+            for (int i = 0; i <= p.GetCount()-1; i++)
             {
 
                 coeffp = p.P[i].GetCoeff();
                 expop = p.P[i].GetExpo();
 
 
-                for (int l = 0; l <=q.GetCount(); l ++)
+                for (int l = 0; l <= q.GetCount(); l++)
                 {
                     Term temp1 = new Term(q.P[l].GetCoeff(), q.P[l].GetExpo());
 
@@ -387,8 +449,9 @@ namespace Task3
                     expoq = q.P[l].GetExpo();
 
                     tempC = coeffp * coeffq;
-                    tempE = expop+expoq;
-                    Term temp3 = new Term(tempC, tempE); 
+                    tempE = expop + expoq;
+                    Term temp3 = new Term(tempC, tempE);
+
 
 
                     hold2.AddTerm(temp3);
@@ -396,29 +459,31 @@ namespace Task3
 
             }
 
-            return hold2; 
+            int m = hold2.GetCount();
+            double zero = hold2.P[m].GetCoeff();
+            
 
-        }
-
-
-
-        public double Evaluate(double x)
-        {
-            double answer = 0;
-
-
-            for (int i = 0; i <= count; i++)
+            do
             {
 
+              zero = hold2.P[m].GetCoeff();
 
-                Double termans = Math.Pow(x, P[i].GetExpo()) * P[i].GetCoeff();
-                answer += termans;
-            }
+                if (zero !=0)
+                {
+                    hold2.P[m].SetCoeff((hold2.P[m].GetCoeff())/2);
+                }
+
+                
+
+              
 
 
+                m--;
+            } while (zero == 0);
 
 
-            return answer;
+            return hold2;
+
         }
 
 
@@ -431,7 +496,7 @@ namespace Task3
                 {
                     Console.Write(P[i]);
 
-                    if ((i != count) && (P[i + 1].GetCoeff() != 0))
+                    if ((i != count)&& (P[i+1].GetCoeff() != 0))
                     {
                         Console.Write(" + ");
                     }
@@ -440,7 +505,7 @@ namespace Task3
             }
 
         }
-
+           
 
     }
 
@@ -458,42 +523,45 @@ public class Node<T>
         Next = next;
     }
 }
-    public class Polynomial
-    {
-        // A reference to the first node of a singly linked list 
-        private Node<Term> front;
+public class Polynomial
+{
+    // A reference to the first node of a singly linked list 
+    private Node<Term> front;
     // Creates the polynomial 
     public Polynomial()
     {
+
     }
+
+
+
+    //Inserts term t into the current polynomial in its proper order, if a term with the same exponent already exisits then the two terms are added together
+    public void AddTerm(Term t)
+    {
+        Node<item> toAddf = new Node<item>();
+
+    }
+    //Adds polynomials p and q to yield a new polynomial 
+    public static Polynomial operator +(Polynomial p, Polynomial q)
+    {
+        // We have to code this 
+    }
+
+    // Multiplies polynomials p and q to yeild a new polynomial 
+    public static Polynomial operator *(Polynomial p, Polynomial q)
+    {
+        // We need to code this
+    }
+    // Evaluates the current polynomial at x 
+    public double Evaluate(double x)
+    {
+        // We need to code this
+    }
+    // Prints current polynomial 
+    public void Print()
+    {
+        // We need to code this
+    }
+}
+
     */
-
-/*
-        //Inserts term t into the current polynomial in its proper order, if a term with the same exponent already exisits then the two terms are added together
-        public void AddTerm(Term t)
-        {
-\\ We have to code this
-        }
-//Adds polynomials p and q to yield a new polynomial 
-    public static Polynomial operator +(Polynomial p, Polynomial q 
-{
-// We have to code this 
-}
-/*
-// Multiplies polynomials p and q to yeild a new polynomial 
-public static Polynomial operator *(Polynomial p, Polynomial q)
-{
-    // We need to code this
-}
-// Evaluates the current polynomial at x 
-public double Evaluate(double x)
-{
-    // We need to code this
-}
-// Prints current polynomial 
-public void Print()
-{
-    // We need to code this
-}
-*/
-
